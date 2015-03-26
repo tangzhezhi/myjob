a43890662ee3755d53a38e65670c0369d9a42ce2
tangzhezi <330882908@qq.com>
2015-03-26 23:09:22 +0800

    异常

--- myweb/web/resources/myjob_js/common.js
+++ myweb/web/resources/myjob_js/common.js
@@ -2,32 +2,52 @@
  * Created by Administrator on 2015/3/24.
  */
 $(function(){
+
+    function alert_message(msg){
+        $().message({
+            type: 'danger',
+            html: "<div class='row'><p class='text-center'>"+msg+"</p></div>",
+            icon: true,
+            position: 'top',
+            autoHide: 4000,
+            beforeFunc: function () {
+                // code to execute before plugin execution
+            },
+            afterFunc: function () {
+                // code to execute after plugin execution
+            },
+            onCloseFunc: function () {
+                // code to execute on close event of message box
+            }
+        });
+    }
+
     // 设置jQuery Ajax全局的参数
     $.ajaxSetup({
         type: "POST",
         error: function(jqXHR, textStatus, errorThrown){
             switch (jqXHR.status){
                 case(500):
-                    alert("服务器系统内部错误");
+                    alert_message("服务器系统内部错误");
                     break;
                 case(401):
-                    alert("未登录");
+                    alert_message("未登录");
                     break;
                 case(403):
-                    alert("无权限执行此操作");
+                    alert_message("无权限执行此操作");
                     break;
                 case(408):
-                    alert("请求超时");
+                    alert_message("请求超时");
                     break;
                 case(404):
-                    alert("没有找到资源");
+                    alert_message("没有找到资源");
                     break;
                 default:
-                    alert("未知错误");
+                    alert_message("未知错误");
             }
         },
         success: function(data){
-            alert("操作成功");
+            alert_message("操作成功");
         },
         complete: function(XMLHttpRequest, textStatus) {
             console.log(XMLHttpRequest);
