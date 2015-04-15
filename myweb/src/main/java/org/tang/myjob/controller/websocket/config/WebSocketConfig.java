a987f497526d5d9b5f646f70a889bc4fd88a600b
330882908@qq.com <330882908@qq.com>
2015-04-15 09:09:31 +0800

    提交

---
+++ myweb/src/main/java/org/tang/myjob/controller/websocket/config/WebSocketConfig.java
@@ -0,0 +1,39 @@
+package org.tang.myjob.controller.websocket.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.socket.WebSocketHandler;
+import org.springframework.web.socket.config.annotation.EnableWebSocket;
+import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
+import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
+import org.springframework.context.annotation.Bean;
+import org.tang.myjob.controller.websocket.hndler.SystemWebSocketHandler;
+import org.tang.myjob.controller.websocket.interceptor.WebSocketHandshakeInterceptor;
+
+@Configuration
+@EnableWebMvc
+@EnableWebSocket
+public class WebSocketConfig extends WebMvcConfigurerAdapter implements
+        WebSocketConfigurer {
+
+    public WebSocketConfig() {
+    }
+
+    @Override
+    public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
+        registry.addHandler(systemWebSocketHandler(), "/websck").addInterceptors(new WebSocketHandshakeInterceptor());
+
+        System.out.println("registed!");
+        registry.addHandler(systemWebSocketHandler(), "/sockjs/websck/info").addInterceptors(new WebSocketHandshakeInterceptor())
+                .withSockJS();
+
+    }
+
+    @Bean
+    public WebSocketHandler systemWebSocketHandler() {
+        //return new InfoSocketEndPoint();
+        return new SystemWebSocketHandler();
+    }
+
+}
---
+++ myweb/src/main/java/org/tang/myjob/controller/websocket/hndler/SystemWebSocketHandler.java
@@ -0,0 +1,116 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.tang.myjob.controller.websocket.hndler;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+import org.springframework.web.socket.CloseStatus;
+import org.springframework.web.socket.TextMessage;
+import org.springframework.web.socket.WebSocketHandler;
+import org.springframework.web.socket.WebSocketMessage;
+import org.springframework.web.socket.WebSocketSession;
+import org.tang.myjob.controller.websocket.Constants;
+
+import java.io.IOException;
+import java.util.ArrayList;
+
+/**
+ *
+ * @author dayu
+ */
+@Component
+public class SystemWebSocketHandler implements WebSocketHandler {
+
+    private static final Logger logger;
+
+    private static final ArrayList<WebSocketSession> users;
+
+    static {
+        users = new ArrayList();
+        logger = LoggerFactory.getLogger(SystemWebSocketHandler.class);
+    }
+
+//    @Autowired
+//    private WebSocketService webSocketService;
+
+    @Override
+    public void afterConnectionEstablished(WebSocketSession session) throws Exception {
+        logger.debug("connect to the websocket success......");
+        users.add(session);
+        String userName = (String) session.getAttributes().get(Constants.WEBSOCKET_USERNAME);
+        if(userName!= null){
+            //查询未读消息
+//            int count = webSocketService.getUnReadNews((String) session.getAttributes().get(Constants.WEBSOCKET_USERNAME));
+            session.sendMessage(new TextMessage("你有未读消息"));
+        }
+    }
+
+    @Override
+    public void handleMessage(WebSocketSession session, WebSocketMessage<?> message) throws Exception {
+
+    }
+
+    @Override
+    public void handleTransportError(WebSocketSession session, Throwable exception) throws Exception {
+        if(session.isOpen()){
+            session.close();
+        }
+        logger.debug("websocket connection closed......");
+        users.remove(session);
+    }
+
+    @Override
+    public void afterConnectionClosed(WebSocketSession session, CloseStatus closeStatus) throws Exception {
+        logger.debug("websocket connection closed......");
+        users.remove(session);
+    }
+
+    @Override
+    public boolean supportsPartialMessages() {
+        return false;
+    }
+
+
+    /**
+     * 给所有在线用户发送消息
+     *
+     * @param message
+     */
+    public void sendMessageToUsers(TextMessage message) {
+        for (WebSocketSession user : users) {
+            try {
+                if (user.isOpen()) {
+                    user.sendMessage(message);
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    /**
+     * 给某个用户发送消息
+     *
+     * @param userName
+     * @param message
+     */
+    public void sendMessageToUser(String userName, TextMessage message) {
+        for (WebSocketSession user : users) {
+            if (user.getAttributes().get(Constants.WEBSOCKET_USERNAME).equals(userName)) {
+                try {
+                    if (user.isOpen()) {
+                        user.sendMessage(message);
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                break;
+            }
+        }
+    }
+    
+}
